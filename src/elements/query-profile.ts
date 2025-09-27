a482f5645b2ede2cf3893d58352a8503265ef24c
Unnamed Shadow <anonymous@example.com>
2025-09-27 10:58:47 +0200

    added user deletion

--- src/elements/query-profile.ts
+++ src/elements/query-profile.ts
@@ -3,40 +3,49 @@ import { Debouncer, handled, log } from "../form-utils"
 import { BaseElement, html } from "./base"
 export default class QueryProfile extends BaseElement {
     static override observedAttributes = [...super.observedAttributes, 'jwt', 'username', 'email', 'id', 'loaded'];
-    debouncer = new Debouncer<{ sub: number }, { ok: boolean, text: string, sub: number }>(async ({ sub }) => {
-        const response = await handled(fetch(`${API}/user/${sub}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
+    debouncer = new Debouncer<{ jwt: string, method: 'GET' | 'DELETE' }, { method: 'GET' | 'DELETE', ok: boolean, text: string, sub: number }>(
+        async ({ jwt, method }) => {
+            const { sub }: { sub: number } = JSON.parse(atob(jwt.split('.')[1]))
+            const response = await handled(fetch(`${API}/user/${sub}`, {
+                method,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + jwt,
+                }
+            }), 'network')
+            return { method, ok: response.ok, text: await handled(response.text(), 'response'), sub }
+        }, ({ ok, text, sub, method }) => {
+            if (!ok) log(text, 'data')
+            else {
+                if (method === 'GET') {
+                    const { username, email } = JSON.parse(text)
+                    this.setAttribute('username', username)
+                    this.setAttribute('email', email)
+                    this.setAttribute('id', sub.toString())
+                }
             }
-        }), 'network')
-        return { ok: response.ok, text: await handled(response.text(), 'response'), sub }
-    }, ({ ok, text, sub }) => {
-        if (!ok) log(text, 'data')
-        else {
-            const { username, email } = JSON.parse(text)
-            this.setAttribute('username', username)
-            this.setAttribute('email', email)
-            this.setAttribute('id', sub.toString())
-        }
-        this.toggleAttribute('loaded', true)
-    })
+            this.toggleAttribute('loaded', true)
+            if (method === 'DELETE') {
+                localStorage.removeItem('jwt')
+                location.reload()
+            }
+        })
     render() {
         if (this.hasAttribute('loaded'))
             return html`<slot></slot>`
         else
-            return html`
-                <iframe src="https://giphy.com/embed/RgzryV9nRCMHPVVXPV" width="200" height="200" style="" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-                <p><a href="https://giphy.com/gifs/trippy-abstract-pi-slices-RgzryV9nRCMHPVVXPV">loader via GIPHY</a></p>
-            `
+            return html`Loading...`
     }
     attachCallbacks() {
         if (this.hasAttribute('loaded') || !this.hasAttribute('jwt')) return
-        const { sub }: { sub: number } = JSON.parse(atob(this.getAttribute('jwt')!.split('.')[1]))
-        this.debouncer.run({ sub }, { delay_before: 50, delay_after: 500, memoize: true })
+        this.debouncer.run({ jwt: this.getAttribute('jwt')!, method: 'GET' }, { delay_before: 50, delay_after: 500, memoize: true })
     }
     update() {
         this.toggleAttribute('loaded', false)
+    }
+    delete() {
+        if (window.prompt('Are you sure? (type \'Yes\')', 'No') === 'Yes')
+            this.debouncer.run({ jwt: this.getAttribute('jwt')!, method: 'DELETE' }, { delay_after: 200 })
     }
 }
 customElements.define('query-profile', QueryProfile)
